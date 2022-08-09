a9dee801df08b4d3823c7f83923c9842e3ed9f48
Grim-Amble <elijah@wesnet.link>
2022-08-09 20:36:17 +1000

    Restart

--- src/main/kotlin/com/lambda/client/setting/GenericConfig.kt
+++
@@ -1,9 +0,0 @@
-package com.lambda.client.setting
-
-import com.lambda.client.setting.configs.NameableConfig
-import com.lambda.client.util.FolderUtils
-
-internal object GenericConfig : NameableConfig<GenericConfigClass>(
-    "generic",
-    "${FolderUtils.lambdaFolder}config/"
-)
